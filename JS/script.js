5ab23c0af5cc06f5df1ba3bbd1f732fd6e8e7938
NguyenAnhToi <toidxbp02gmail.com>
2024-05-04 23:27:42 +0700

    Day-5

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+//----------------------Menu-item---------------
+
+const header = document.querySelector("header")
+window.addEventListener("scroll", function () {
+    x = window.pageYOffset
+    if (x > 0) {
+        header.classList.add("sticky")
+    }
+    else {
+        header.classList.remove("sticky")
+    }
+})
+
+//-------------------------Menu-Slider-Bar------------------------
+const itemsliderbar = document.querySelectorAll(".category-left-li")
+itemsliderbar.forEach(function (menu, index) {
+    menu.addEventListener("click", function () {
+        menu.classList.toggle("block")
+    })
+})
